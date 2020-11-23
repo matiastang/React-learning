33dbfd572de7eaf3a11a7a6f9882acb2da4a81d3
matiastang <18380449615@163.com>
2020-11-23 23:28:40 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -2,17 +2,42 @@
  * @Author: tangdaoyong
  * @Date: 2020-11-23 22:26:28
  * @LastEditors: tangdaoyong
- * @LastEditTime: 2020-11-23 22:34:21
+ * @LastEditTime: 2020-11-23 23:26:53
  * @Description: file content
  */
 import _ from 'lodash';
+import './css/index.css';
+import Icon from './images/icon.png';
+import Data from './resources/data.xml';
+import Notes from './resources/data.csv';
+import toml from './resources/data.toml';
+import yaml from './resources/data.yaml';
+import json from './resources/data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
   
     // lodash，现在通过一个 script 引入
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+    // 将图像添加到我们已经存在的 div 中。
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
+    
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,63 @@
  * @Author: tangdaoyong
  * @Date: 2020-11-23 22:39:32
  * @LastEditors: tangdaoyong
- * @LastEditTime: 2020-11-23 22:40:19
+ * @LastEditTime: 2020-11-23 23:25:58
  * @Description: file content
  */
 const path = require('path');
+const toml = require('toml');
+const yaml = require('yamljs');
+const json5 = require('json5');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+      rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(csv|tsv)$/i,
+            use: ['csv-loader'],
+          },
+          {
+            test: /\.xml$/i,
+            use: ['xml-loader'],
+          },
+          {
+            test: /\.toml$/i,
+            type: 'json',
+            parser: {
+              parse: toml.parse,
+            },
+          },
+          {
+            test: /\.yaml$/i,
+            type: 'json',
+            parser: {
+              parse: yaml.parse,
+            },
+          },
+          {
+            test: /\.json5$/i,
+            type: 'json',
+            parser: {
+              parse: json5.parse,
+            },
+          },
+      ]
+  }
 };
